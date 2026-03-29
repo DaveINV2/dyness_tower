44992305df422df55bc8eb73fc180855b909e741
DaveINV2 <149502924+DaveINV2@users.noreply.github.com>
2026-03-29 20:15:50 +0200

    update sensors

--- custom_components/dyness_battery/sensor.py
+++ custom_components/dyness_battery/sensor.py
@@ -1,9 +1,6 @@
 """Sensors for Dyness Battery Integration."""
 from homeassistant.components.sensor import SensorEntity, SensorDeviceClass, SensorStateClass
-from homeassistant.const import (
-    PERCENTAGE, UnitOfPower, UnitOfElectricCurrent, UnitOfEnergy,
-    UnitOfTemperature, UnitOfElectricPotential,
-)
+from homeassistant.const import PERCENTAGE, UnitOfPower, UnitOfElectricCurrent, UnitOfEnergy, UnitOfTemperature, UnitOfElectricPotential
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from . import DOMAIN
@@ -14,7 +11,10 @@ SENSORS = [
     ("soc", "battery_soc", PERCENTAGE, SensorDeviceClass.BATTERY, SensorStateClass.MEASUREMENT, "mdi:battery-high", None, None),
     ("realTimePower", "battery_power", UnitOfPower.WATT, SensorDeviceClass.POWER, SensorStateClass.MEASUREMENT, "mdi:lightning-bolt", None, None),
     ("realTimeCurrent", "battery_current", UnitOfElectricCurrent.AMPERE, SensorDeviceClass.CURRENT, SensorStateClass.MEASUREMENT, "mdi:current-dc", None, None),
+    ("packVoltage", "pack_voltage", UnitOfElectricPotential.VOLT, SensorDeviceClass.VOLTAGE, SensorStateClass.MEASUREMENT, "mdi:sine-wave", 3, None),
     ("soh", "battery_soh", PERCENTAGE, SensorDeviceClass.BATTERY, SensorStateClass.MEASUREMENT, "mdi:battery-heart", None, None),
+    ("cellVoltageMax", "cell_voltage_max", UnitOfElectricPotential.VOLT, SensorDeviceClass.VOLTAGE, SensorStateClass.MEASUREMENT, "mdi:sine-wave", 3, None),
+    ("cellVoltageMin", "cell_voltage_min", UnitOfElectricPotential.VOLT, SensorDeviceClass.VOLTAGE, SensorStateClass.MEASUREMENT, "mdi:sine-wave", 3, None),
     ("cellVoltageDiffMv", "cell_voltage_diff_mv", "mV", None, SensorStateClass.MEASUREMENT, "mdi:arrow-expand-horizontal", 1, None),
     ("chargeLimit", "charge_limit", UnitOfElectricCurrent.AMPERE, SensorDeviceClass.CURRENT, SensorStateClass.MEASUREMENT, "mdi:battery-arrow-up", None, None),
     ("dischargeLimit", "discharge_limit", UnitOfElectricCurrent.AMPERE, SensorDeviceClass.CURRENT, SensorStateClass.MEASUREMENT, "mdi:battery-arrow-down", None, None),
@@ -30,11 +30,10 @@ ALWAYS_REGISTER = {"soc", "realTimePower", "realTimeCurrent", "batteryCapacity",
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator = hass.data[DOMAIN][entry.entry_id]
     async_add_entities([DynessSensor(coordinator, entry, *s) for s in SENSORS if s[0] in ALWAYS_REGISTER or coordinator.data.get(s[0]) is not None])
-    
     known_module_ids = set()
     def _add_new_modules():
-        module_data = (coordinator.data or {}).get("module_data", {})
-        new_mids = [mid for mid in module_data if mid not in known_module_ids]
+        m_data = (coordinator.data or {}).get("module_data", {})
+        new_mids = [mid for mid in m_data if mid not in known_module_ids]
         for mid in new_mids:
             known_module_ids.add(mid)
             async_add_entities([DynessModuleSensor(coordinator, entry, mid, *s) for s in MODULE_SENSORS])
@@ -42,14 +41,13 @@ async def async_setup_entry(hass, entry, async_add_entities):
     entry.async_on_unload(coordinator.async_add_listener(_add_new_modules))
 
 class DynessSensor(CoordinatorEntity, SensorEntity):
-    def __init__(self, coordinator, entry, key, trans_key, unit, dev_cls, state_cls, icon, prec, cat):
-        super().__init__(coordinator)
-        self._key, self._attr_translation_key, self._attr_native_unit_of_measurement = key, trans_key, unit
-        self._attr_device_class, self._attr_state_class, self._attr_icon = dev_cls, state_cls, icon
+    def __init__(self, coord, entry, key, trans, unit, dev, state, icon, prec, cat):
+        super().__init__(coord)
+        self._key, self._attr_translation_key, self._attr_native_unit_of_measurement = key, trans, unit
+        self._attr_device_class, self._attr_state_class, self._attr_icon = dev, state, icon
         self._attr_unique_id, self._attr_has_entity_name = f"{entry.entry_id}_{key}", True
         if prec: self._attr_suggested_display_precision = prec
         if cat: self._attr_entity_category = cat
-
     @property
     def native_value(self): return self.coordinator.data.get(self._key)
 
@@ -57,18 +55,16 @@ MODULE_SENSORS = [
     ("soc", "module_soc", PERCENTAGE, SensorDeviceClass.BATTERY, SensorStateClass.MEASUREMENT, "mdi:battery-high", None),
     ("cell_temp_1", "module_temp_1", UnitOfTemperature.CELSIUS, SensorDeviceClass.TEMPERATURE, SensorStateClass.MEASUREMENT, "mdi:thermometer", None),
     ("cell_temp_2", "module_temp_2", UnitOfTemperature.CELSIUS, SensorDeviceClass.TEMPERATURE, SensorStateClass.MEASUREMENT, "mdi:thermometer", None),
-    # Individual Cell Voltages (01-30)
     *[(f"cell_{i:02d}", f"cell_{i:02d}", UnitOfElectricPotential.VOLT, SensorDeviceClass.VOLTAGE, SensorStateClass.MEASUREMENT, "mdi:battery-outline", 3) for i in range(1, 31)],
 ]
 
 class DynessModuleSensor(CoordinatorEntity, SensorEntity):
-    def __init__(self, coordinator, entry, module_id, key, trans_key, unit, dev_cls, state_cls, icon, prec=None):
-        super().__init__(coordinator)
-        self._module_id, self._key, self._attr_translation_key = module_id, key, trans_key
-        self._attr_native_unit_of_measurement, self._attr_device_class = unit, dev_cls
-        self._attr_state_class, self._attr_icon, self._attr_has_entity_name = state_cls, icon, True
-        self._attr_unique_id = f"{entry.entry_id}_{module_id}_{key}"
+    def __init__(self, coord, entry, mid, key, trans, unit, dev, state, icon, prec=None):
+        super().__init__(coord)
+        self._mid, self._key, self._attr_translation_key = mid, key, trans
+        self._attr_native_unit_of_measurement, self._attr_device_class = unit, dev
+        self._attr_state_class, self._attr_icon, self._attr_has_entity_name = state, icon, True
+        self._attr_unique_id = f"{entry.entry_id}_{mid}_{key}"
         if prec: self._attr_suggested_display_precision = prec
-
     @property
-    def native_value(self): return self.coordinator.data.get("module_data", {}).get(self._module_id, {}).get(self._key)
+    def native_value(self): return self.coordinator.data.get("module_data", {}).get(self._mid, {}).get(self._key)
